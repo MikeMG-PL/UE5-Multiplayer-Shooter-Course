0b4174c3b61d13cf475b1831d5564b73ebeb0b90
Michał Galiński <galinski.06@vp.pl>
2022-08-24 21:37:44 +0200

    Weapon class + widget

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -11,7 +11,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
-	if(NumberOfPlayers == 1)
+	if(NumberOfPlayers == 1) // CHANGE TO 2!
 	{
 		UWorld* World = GetWorld();
 		if(World)
